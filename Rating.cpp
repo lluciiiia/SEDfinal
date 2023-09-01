0a520fd0d9088225ffe6476d1327493fbfd8748d
HoangDuy <49156225+LazDude@users.noreply.github.com>
2023-09-01 20:24:24 +0700

    Update Rating.cpp

--- Rating.cpp
+++ Rating.cpp
@@ -1,3 +1,29 @@
+
 // Rating
+class Rating {
+    protected:
+    float score;
+    string comment;
+};
+
 // UserRating
+class UserRating : protected Rating{
+    string username;
+    public:
+    UserRating(string username, float score, string comment) {
+        this->username = username;
+        this->score = score;
+        this->comment = comment;
+    }
+};
+
 // BikeRating
+class MotorbikeRating : protected Rating{
+    string bikeID;
+    public:
+    MotorbikeRating(string bikeID, float score, string comment) {
+        this->bikeID = bikeID;
+        this->score = score;
+        this->comment = comment;
+    }
+};
