d5409c0af09ad0a676d8fcc151dbf488dbcc1644
HoangDuy <49156225+LazDude@users.noreply.github.com>
2023-09-01 21:24:21 +0700

    Added constructor methods

--- Rating.cpp
+++ Rating.cpp
@@ -1,32 +1,83 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "User.cpp" // I'm not sure if it's .cpp or .h
 #ifndef RATING_H
 #define RATING_H
-using namespace std;
 
-// Rating
-class Rating {
-    protected:
+class Rating
+{
+protected:
     float score;
-    string comment;
+    std::string comment;
+    std::string userID;
 };
 
-// UserRating
-class UserRating : protected Rating {
-    string username;
-    public:
-    UserRating(string username, float score, string comment) {
+class UserRating : public Rating
+{
+    std::string username;
+
+public:
+    // Constructors
+    UserRating();
+    UserRating(std::string username, float score, std::string comment)
+    {
         this->username = username;
         this->score = score;
         this->comment = comment;
     }
+
+    // Getters/setters
+    int getScore() {
+        return this->score;
+    }
+
+    void setScore(int score) {
+        this->score = score;
+    }
+
+    std::string getComment() {
+        return this->comment;
+    }
+
+    void setComment(std::string comment) {
+        this->comment = comment;
+    }
+
+    // Missing the user class so it's giving an error
+    int getNumberOfRatings() {
+        return userRating->size;
+    }
 };
 
-// BikeRating
-class MotorbikeRating : protected Rating {
-    string bikeID;
-    public:
-    MotorbikeRating(string bikeID, float score, string comment) {
+class MotorbikeRating : public Rating
+{
+    std::string bikeID;
+
+public:
+    // Constructors 
+    MotorbikeRating();
+    MotorbikeRating(std::string bikeID, float score, std::string comment)
+    {
         this->bikeID = bikeID;
         this->score = score;
+        this->comment = comment;
+    }
+
+    // Getters/setters
+    int getScore() {
+        return this->score;
+    }
+
+    void setScore (int score) {
+        this->score = score; 
+    }
+
+    std::string getComment() {
+        return this->comment;
+    }
+
+    void setComment(std::string comment) {
         this->comment = comment;
     }
 };
