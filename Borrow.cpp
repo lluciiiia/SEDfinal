f45453f9688cf50b2d8d41d76626e58dbfab33eb
Kim Minsung <s3866724@rmit.edu.vn>
2023-09-02 07:22:55 +0700

    .

---
+++ Borrow.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include "TimeSlot.cpp"
+
+using namespace std;
+
+class Borrow {
+private:
+    TimeSlot timeSlot;
+    string username;
+    int motorbikeId;
+    double price;
+
+public:
+    Borrow(TimeSlot timeSlot, string username, int motorbikeId, double price)
+        : timeSlot(timeSlot), username(username), motorbikeId(motorbikeId), price(price) {}
+
+    TimeSlot getTimeSlot() const {
+        return timeSlot;
+    }
+
+    string getUsername() const {
+        return username;
+    }
+
+    int getMotorbikeID() const {
+        return motorbikeId;
+    }
+
+    double getPrice() const {
+        return price;
+    }
+};
---
+++ TimeSlot.cpp
@@ -0,0 +1,27 @@
+#ifndef TIMESLOT_H
+#define TIMESLOT_H
+#include <string>
+
+using namespace std;
+
+class TimeSlot {
+private:
+    string startTime;
+    string endTime;
+
+public:
+    TimeSlot(const string& startTime, const string& endTime)
+        : startTime(startTime), endTime(endTime) {
+    }
+
+    string getStartTime() const {
+        return startTime;
+    }
+
+    string getEndTime() const {
+        return endTime;
+    }
+};
+
+
+#endif
