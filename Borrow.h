dc4a1ab648ed695eac50d879a33dddcb94e4919f
Kim Minsung <s3866724@rmit.edu.vn>
2023-09-03 20:26:56 +0700

    .

---
+++ Borrow.h
@@ -0,0 +1,50 @@
+#include <string>
+#include "TimeSlot.h"
+
+using namespace std;
+
+class Borrow {
+private:
+    TimeSlot timeSlot;
+    string username;
+    string motorbikeId;
+    double price;
+
+public:
+    Borrow(TimeSlot timeSlot, string username, string motorbikeId, double price)
+        : timeSlot(timeSlot), username(username), motorbikeId(motorbikeId), price(price) {}
+
+    //setters
+    void setTimeSlot(TimeSlot timeSlot) {
+        timeSlot = timeSlot;
+    }
+
+    void setUsername(string username) {
+        username = username;
+    }
+
+    void setMotorbikeId(string motorbikeId) {
+        motorbikeId = motorbikeId;
+    }
+
+    void setPrice(double price) {
+        price = price;
+    }
+    
+    //getters
+    TimeSlot getTimeSlot() const {
+        return timeSlot;
+    }
+
+    string getUsername() const {
+        return username;
+    }
+
+    string getMotorbikeID() const {
+        return motorbikeId;
+    }
+
+    double getPrice() const {
+        return price;
+    }
+};
