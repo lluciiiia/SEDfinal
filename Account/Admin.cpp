558a6f262a3a79acb5a1828d45171849c5ff4e44
HoangDuy <49156225+LazDude@users.noreply.github.com>
2023-09-10 03:23:49 +0700

    Implemented three view methods (Might need adjust)

--- Account/Admin.cpp
+++ Account/Admin.cpp
@@ -19,16 +19,51 @@ Admin::Admin(string username,
     this->adminID = adminID;
 }
 
-void Admin::viewMemberInfo()
-{
+//Might change parameter -> vector
+void Admin::viewMemberInfo(User user)
+{    
+    //Print userID, IDtype, licenseNumber, licenseExpiryDate, creditPoits, city 
+    //(Might add motorbike, motorbikeRatings and userRatings if necessary)
+    cout << "\nMember information:\n";
+    cout << "User ID: " << user.userID << endl;
+    cout << "ID Type: " << user.IDtype << endl;
+    cout << "License Number: " << user.licenseNumber << endl;
+    cout << "License Expiry Date: " << user.licenseExpiryDate << endl;
+    cout << "Credit points: " << user.creditPoint << endl;
+    cout << "City: " << user.city << endl;
+    cout << "\n";    
 }
 
-void Admin::viewMotorbikeInfo()
+//Make change parameter -> vector
+void Admin::viewMotorbikeInfo(Motorbike motorbike)
 {
+    //model, motorbikeID, color, engineSize, city, owner, transmissionMode, yearMade , description, consumingPoints, rentalAmount, motorbikeRating, requests, availability
+    cout << "\nMotorbike information:\n";
+    cout << "Model: " << motorbike.model << endl;
+    cout << "Motorbike ID: " << motorbike.motorbikeID << endl;
+    cout << "Color: " << motorbike.color << endl; 
+    cout << "Engine size: " << motorbike.engineSize << endl;
+    cout << "City: " << motorbike.city << endl;
+    cout << "Owner ID: << motorbike.owner.userID << endl;
+    cout << "Transmission mode: " << motorbike.transmissionMode << endl;
+    cout << "Year made: " << motorbike.yearMade << endl;
+    cout << "Description: " << motorbike.description << endl;
+    cout << "Consuming points: " << motorbike.consumingPoints << endl;
+    cout << "Rental amount: " << motorbike.rentalAmount << endl;
+    cout << "Rating: " << motorbike.motorbikeRating << endl;
+    cout << "Availability: " << motorbike.availability << endl;
 }
 
-void Admin::viewAccounts()
+//View a specific account (Might change parameter -> vector)
+void Admin::viewAccounts(Account account)
 {
+    //username, password, fullName, phoneNumber
+    cout << "\nAccount information:\n";
+    cout << "Username: " << account.getUsername() << endl;
+    cout << "Password: " << account.getPassword() << endl;
+    cout << "Full name: " << account.getFullName() << endl;
+    cout << "Phone number: " << account.getPhoneNumber() << endl;
+    cout << "\n";
 }
 
 string Admin::getAdminID() const
