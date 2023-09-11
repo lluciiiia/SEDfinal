d3130c56b535b509522b1744802546bcc4643f39
HoangDuy <49156225+LazDude@users.noreply.github.com>
2023-09-11 22:44:31 +0700

    Update Motorbike.cpp

--- Motorbike.cpp
+++ Motorbike.cpp
@@ -1,54 +1,53 @@
 #include "Motorbike.h"
-#include "Account/User.h"
+
 
 using namespace std;
 
-Motorbike::Motorbike(string model,
-                     string motorbikeID,
-                     string color,
-                     string engineSize,
-                     City city,
-                     User owner,
-                     string transmissionMode,
-                     int yearMade,
-                     string description,
-                     double consumingPoints,
-                     double rentalAmount,
-                     // double minRenterRating,
-                     vector<MotorbikeRating> motorbikeRatings,
-                     vector<Request> motorbikeRequests,
-                     bool availability)
-
-{
-        this->model = model;
-        this->motorbikeID = motorbikeID;
-        this->color = color;
-        this->engineSize = engineSize;
-        this->city = city;
-        this->transmissionMode = transmissionMode;
-        this->yearMade = yearMade;
-        this->description = description;
-        this->consumingPoints = consumingPoints;
-        this->rentalAmount = rentalAmount;
-        // this->minRenterRating = minRenterRating;
-        this->motorbikeRatings = motorbikeRatings;
-        this->motorbikeRequests = motorbikeRequests;
-        this->availability = availability;
-        // this-> ownerUsername= owner;
-        // this-> renterUsername= renter;
-}
 
 // TimeSlot Motorbike::getTimeSlot()
 // {
 //         return
 // }
 
-User Motorbike::getOwner()
+Motorbike::Motorbike(string model, int motorbikeID, string color, string engineSize, City city, string owner, string transmissionMode, int yearMade, string description, double consumingPoints, double rentalAmount, double minRenterRating, double motorbikeRating)
+{
+        this->model= model;
+        this->motorbikeID= motorbikeID;
+        this->color= color;
+        this->engineSize=engineSize;
+        this->city=city;
+        this->owner= owner;
+        this->transmissionMode= transmissionMode;
+        this->yearMade=yearMade;
+        this->description=description;
+        this->consumingPoints=consumingPoints;
+        this->rentalAmount=rentalAmount;
+        this->minRenterRating=minRenterRating;
+        this->motorbikeRating=motorbikeRating;
+}
+
+Motorbike::Motorbike(string model, int motorbikeID, string color, string engineSize, City city, string owner, string transmissionMode, int yearMade, string description, double consumingPoints, double rentalAmount)
+{
+        this->model = model;
+        this->motorbikeID=motorbikeID;
+        this->color=color;
+        this->engineSize= engineSize;
+        this->city= city;
+        this->owner= owner;
+        this-> transmissionMode = transmissionMode;
+        this->yearMade = yearMade;
+        this->description=description;
+        this->consumingPoints= consumingPoints;
+        this->rentalAmount;
+}
+
+string Motorbike::getOwner()
+
 {
         return owner;
 }
 
-string Motorbike::getMotorbikeId()
+int Motorbike::getMotorbikeId()
 {
         return motorbikeID;
 }
@@ -58,22 +57,19 @@ double Motorbike::getConsumingPoints()
         return consumingPoints;
 }
 
-bool Motorbike::getAvailability()
-{
-        return availability;
-}
-
 void Motorbike::setAvailability(bool availability)
 {
         this->availability = availability;
 }
 
-void Motorbike::setOwner(User owner)
+void Motorbike::setOwner(string owner)
 {
         this->owner = owner;
 }
 
-void Motorbike::setMotorbikeID(string motorbikeID)
+
+void Motorbike::setMotorbikeID(int motorbikeID)
+
 {
         this->motorbikeID = motorbikeID;
 }
@@ -85,21 +81,40 @@ void Motorbike::setConsumingPoints(double consumingPoints)
 
 void Motorbike::addRequest(const Request &request)
 {
-        motorbikeRequests.push_back(request);
+        requests.push_back(request);
 }
 
-void Motorbike::viewRequest(const Request &request)
+void Motorbike::viewRequests()
+{       int i = 1;
+        cout << "View requests of the motorbike " << motorbikeID << "\n";
+        for (Request request : requests) {
+                cout << "Request " << i << ":\n";
+                request.showInfo();
+        }
+}       
+
+bool Motorbike::checkAvailability(bool availability)
 {
+        return availability;
 }
 
-void Motorbike::addRating(const MotorbikeRating &rating)
-{
-        motorbikeRatings.push_back(rating);
-}
+// void Motorbike::addCreditPoints(double points)
+// {
 
-void Motorbike::reserveTimeSlot(const TimeSlot &timeSlot)
-{
-}
+// }
 
-friend class Admin;
+// string Motorbike::toStringMotorBike()
+// {
+//         return model + "," +
+//                motorbikeID + "," +
+//                color + "," +
+//                engineSize + "," +
+//                transmissionMode + "," +
+//                to_string(yearMade) + "," +
+//                description + "," +
+//                to_string(consumingPoints) + "," +
+//                to_string(rentalAmount) + "," +
+//                to_string(minRenterRating) + "," +
+//                to_string(motorbikeRating) + ",";
+// }
 
