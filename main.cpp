38c2d09e04c1b2f27bf47140c7c4db20628f6205
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2023-09-12 22:36:04 +0700

    Guest dashboard

--- main.cpp
+++ main.cpp
@@ -7,25 +7,23 @@
 // #include "Request.h"
 // #include "TimeSlot.h"
 #include <iostream>
-#include<cstdlib>
+#include <cstdlib>
 #include <iomanip>
 // #include "Borrow.h"
 
 using namespace std;
 
-
-
+void guest_dashboard(vector<Motorbike> &bikes);
 void user_dashboard(User &user, vector<Motorbike> &bikes);
-void viewBikeDash(User &user,vector<Motorbike> &bikes);
+void viewBikeDash(User &user, vector<Motorbike> &bikes);
 int main()
-{   
+{
     User user;
     saveToFile fileSave;
-    vector <Motorbike> motorbikeList=fileSave.loadMotor();
+    vector<Motorbike> motorbikeList = fileSave.loadMotor();
     vector<User> userList = fileSave.loadAccount(motorbikeList);
-    
-     City city= City:: Saigon;
 
+    City city = City::Saigon;
 
     // User user1("john_doe", "password123", "John Doe", "1234567890", "Passport", "ID12345", "License67890", "2023-12-31", 100.0, city);
 
@@ -33,91 +31,161 @@ int main()
 
     // userList.push_back(user1);
     // userList.push_back(user2);
-    
-    cout<< "EEET2482/COSC2082 ASSIGNMENT"<< "\n";
-    cout<< "MOTORBIKE RENTAL APPLICATION."<< "\n\n";
 
-    cout<< "Instructor: Dr. Ling Hou Chong"<< "\n";
-    cout<< "Group: 3"<< "\n";
-    cout<< "s3929218, Student Name: Huynh Tan Phat"<< "\n";
-    cout<< "s3929218, Student Name: Huynh Tan Phat"<< "\n";
-    cout<< "s3929218, Student Name: Huynh Tan Phat"<< "\n";
-    cout<< "s3929218, Student Name: Huynh Tan Phat"<< "\n\n\n";
+    cout << "EEET2482/COSC2082 ASSIGNMENT"
+         << "\n";
+    cout << "MOTORBIKE RENTAL APPLICATION."
+         << "\n\n";
 
-    bool exitProgram= false;
-    
-    while (!exitProgram){
+    cout << "Instructor: Dr. Ling Hou Chong"
+         << "\n";
+    cout << "Group: 3"
+         << "\n";
+    cout << "s3929218, Student Name: Huynh Tan Phat"
+         << "\n";
+    cout << "s3929218, Student Name: Huynh Tan Phat"
+         << "\n";
+    cout << "s3929218, Student Name: Huynh Tan Phat"
+         << "\n";
+    cout << "s3929218, Student Name: Huynh Tan Phat"
+         << "\n\n\n";
+
+    bool exitProgram = false;
+
+    while (!exitProgram)
+    {
         int choice;
-        cout<< "MOTORBIKE RENTAL APPLICATION."<< "\n\n";
-        cout<< "Use the app as 1.Guest  2. Member  3.Admin  4.Register  5.Exit the program\n\n";
-        cin>> choice;
+        cout << "MOTORBIKE RENTAL APPLICATION."
+             << "\n\n";
+        cout << "Use the app as 1.Guest  2. Member  3.Admin  4.Register  5.Exit the program\n\n";
+        cin >> choice;
         cin.ignore();
-        switch(choice){
-            case 1:{
-                break;
+        switch (choice)
+        {
+        case 1:
+        {
+            cout << "Logging in as a Guest \n";
+            cout << "Your are on a Guest Mode.\n";
+
+            guest_dashboard(motorbikeList);
+            system("cls");
+
+            break;
+        }
+        case 2:
+        {
+            cout << "Logging in as a Member\n";
+            cout << "Your are logging in.\n";
+            if (login(user, userList))
+            {
+                user_dashboard(user, motorbikeList);
+                system("cls");
             }
-            case 2:{
-                cout<< "Logging in \n";
-                cout<< "Your are logging in.\n";
-                if(login(user,userList)){
-                    user_dashboard(user,motorbikeList );
-                    system("cls");
-                }else{
-                    cout<< "Login fail! Wrong username or password! \n";
-                }
-                break;
+            else
+            {
+                cout << "Login fail! Wrong username or password! \n";
             }
-            case 3:{
-                break;
+            break;
+        }
+        case 3:
+        {
+            break;
+        }
+        case 4:
+        {
+            if (user.registerAccount(userList))
+            {
+                cout << "Register successfully\n";
             }
-            case 4:{
-                if(user.registerAccount(userList)){
-                    cout<< "Register successfully\n";
-                }else{
-                    cout<< "Fail\n";
-                }
-                break;
+            else
+            {
+                cout << "Fail\n";
             }
-            case 5:{
-                exitProgram= true;
-                cout<< "Bye! See you later!! \n";
-                break;
-            }
+            break;
+        }
+        case 5:
+        {
+            exitProgram = true;
+            cout << "Bye! See you later!! \n";
+            break;
+        }
         }
     }
-
-
-
 
     fileSave.SaveAccountToFile(userList);
     fileSave.SaveMotobikeToFile(motorbikeList);
     return 0;
 }
 
-
-
-
-
-
-
-void user_dashboard(User &user, vector<Motorbike> &bikes){
+void guest_dashboard(vector<Motorbike> &bikes)
+{
     int choice;
-    bool dashboardRun= false;
-    
-    while (!dashboardRun) {
-    
+    bool dashboardRun = false;
+    while (!dashboardRun)
+    {
+
         system("cls");
-        string model;
-         for(auto &bike : bikes){
-        if(bike.getOwner() == user.getUsername()){
-            model=bike.getModel(); 
-        }
-    }
         // Display the user dashboard menu
         cout << "|==========================|\n";
-        cout << "|      User Dashboard      |  Hello, "<< user.getUsername()<< "\n";
-        cout << "|==========================|  Credit point: " << user.getCreditPoint()<< "\n";
-        cout << "| 1. Add your motorbike    |  Your owned bike: " << model <<"\n";
+        cout << "|      Guest Dashboard     |  Hello, Guest\n";
+        cout << "|==========================| \n";
+        cout << "| 1. View Bikes in Ha Noi  |  You can only view motorbikes by cities\n";
+        cout << "| 2. View Bikes in Saigon  |\n";
+        cout << "| 3. Logout                |\n";
+        cout << "|==========================|\n";
+        cout << "Enter your choice (1-3): ";
+
+        cin >> choice;
+        cin.ignore(); // Consume the newline character
+
+        switch (choice)
+        {
+        case 1:
+
+            viewBikeDash(user, bikes);
+
+            break;
+        case 2:
+            viewBikeDash(user, bikes);
+
+            break;
+        case 3:
+
+            dashboardRun = true;
+            cout << "Logging out...\n";
+            break;
+        default:
+            cout << "Invalid choice. Please enter a valid option (1-4).\n";
+            break;
+        }
+        // Pause to allow the user to read the message
+        cout << "Press Enter to continue...";
+        cin.ignore();
+    }
+}
+
+void user_dashboard(User &user, vector<Motorbike> &bikes)
+{
+    int choice;
+    bool dashboardRun = false;
+
+    while (!dashboardRun)
+    {
+
+        system("cls");
+        string model;
+        for (auto &bike : bikes)
+        {
+            if (bike.getOwner() == user.getUsername())
+            {
+                model = bike.getModel();
+            }
+        }
+        // Display the user dashboard menu
+        cout << "|==========================|\n";
+        cout << "|      User Dashboard      |  Hello, " << user.getUsername() << "\n";
+        cout << "|==========================|  Credit point: " << user.getCreditPoint() << "\n";
+        cout << "| 1. Add your motorbike    |  Your owned bike: " << model << "\n";
         cout << "| 2. View Bikes to rent    |\n";
         cout << "| 3. View request          |\n";
         cout << "| 4. Logout                |\n";
@@ -127,45 +195,44 @@ void user_dashboard(User &user, vector<Motorbike> &bikes){
         cin >> choice;
         cin.ignore(); // Consume the newline character
 
-        switch (choice) {
-            case 1:
-                
-                user.addBike(bikes);
-                
-                break;
-            case 2:
-                viewBikeDash(user,bikes);
-               
-                break;
-            case 3:
-                
-            
-                break;
-            case 4:
-                dashboardRun= true;
-                cout << "Logging out...\n";
-                break;
-            default:
-                cout << "Invalid choice. Please enter a valid option (1-4).\n";
-                break;
+        switch (choice)
+        {
+        case 1:
+
+            user.addBike(bikes);
+
+            break;
+        case 2:
+            viewBikeDash(user, bikes);
+
+            break;
+        case 3:
+
+            break;
+        case 4:
+            dashboardRun = true;
+            cout << "Logging out...\n";
+            break;
+        default:
+            cout << "Invalid choice. Please enter a valid option (1-4).\n";
+            break;
         }
 
         // Pause to allow the user to read the message
         cout << "Press Enter to continue...";
         cin.ignore();
     }
-
 }
 
-
-
-void viewBikeDash(User &user,vector<Motorbike> &bikes){
+void viewBikeDash(User &user, vector<Motorbike> &bikes)
+{
     system("cls");
     int choice;
-    bool dashboardRun= false;
+    bool dashboardRun = false;
     cout << left << setw(12) << "Motorbike ID" << setw(20) << "Model" << setw(10) << "Color" << setw(10) << "Engine" << setw(15) << "Owner" << setw(12) << "Year" << setw(20) << "Description" << setw(8) << "Rating" << endl;
     cout << setfill('-') << setw(100) << "-" << setfill(' ') << endl;
-    for ( Motorbike& bike : bikes) {
+    for (Motorbike &bike : bikes)
+    {
         cout << left << setw(12) << bike.getMotorbikeId()
              << setw(20) << bike.getModel()
              << setw(10) << bike.getColor()
@@ -176,15 +243,10 @@ void viewBikeDash(User &user,vector<Motorbike> &bikes){
              << setw(8) << bike.getRating()
              << endl;
     }
-    while (!dashboardRun) {
-        
-        
+    while (!dashboardRun)
+    {
 
-
-
-
-
-        cout<< "Enter your choice: ";
+        cout << "Enter your choice: ";
         cin >> choice;
         cin.ignore();
     }
